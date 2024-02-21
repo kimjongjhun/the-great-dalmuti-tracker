570445864912945753af274d9eaadf8dabc3e994
Jong Kim <kim.jongjhun@gmail.com>
2024-02-20 19:25:56 -0500

    Got basic functionality working.

---
+++ app.py
@@ -0,0 +1,83 @@
+import os
+from datetime import datetime
+from flask import Flask, request, jsonify
+import psycopg2
+from dotenv import load_dotenv
+
+from pgsql import CREATE_ROUNDS_TABLE, GET_ALL_ROUNDS, INSERT_ROUND, DELETE_ROUND, GET_ROUND_BY_ID, UPDATE_ROUND
+
+load_dotenv()
+
+app = Flask(__name__)
+url = os.getenv("DATABASE_URL")
+connection = psycopg2.connect(url)
+
+@app.get("/get-all-rounds")
+def get_all_rounds():
+    rounds = []
+
+    try:
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute(GET_ALL_ROUNDS)
+                rounds = cursor.fetchall()
+    except Exception as e:
+        return jsonify({"error": "Internal Server Error", "message": f"ERROR: {e}"}), 500
+
+    return jsonify({"message": "Rounds successfully fetched", "data": rounds}), 200
+
+@app.post("/add-new-round")
+def add_new_round():
+    data = request.get_json()
+    date = datetime.now().date() if not data.get('date') else data['date']
+        
+    try:
+        results = data['results']
+    except KeyError as e:
+        return jsonify({"error": "Missing info", "message": "Must have round information to save."}), 400
+
+    try:
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute(CREATE_ROUNDS_TABLE)
+                cursor.execute(INSERT_ROUND, (date, results))
+    except Exception as e:
+        return jsonify({"error": "Internal Server Error", "message": f"ERROR: {e}"}), 500
+
+    return jsonify({"message": "Round saved successfully"}), 201
+
+@app.delete("/delete-round/<int:round_id>")
+def delete_round(round_id: int):
+    try:
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute(DELETE_ROUND, (round_id,))
+    except Exception as e:
+        return jsonify({"error": "Internal Server Error", "message": f"ERROR: {e}"}), 500
+
+    return jsonify({"message": "Round successfully deleted"}), 200
+
+@app.patch("/edit-round/<int:round_id>")
+def update_round(round_id: int):
+    data = request.get_json()
+    date = datetime.now().date() if not data.get('date') else data['date']
+            
+    try:
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute(GET_ROUND_BY_ID, (round_id,))
+
+                round = cursor.fetchone()
+                if round:
+                    new_date = round[1] if not data.get('date') else data['date']
+                    new_results = round[2] if not data.get('results') else data['results']
+
+                    cursor.execute(UPDATE_ROUND, (new_date, new_results, round_id))
+                else:
+                    return jsonify({"error": "Request Error", "meesage": "Round to update not found, please check round id"}), 404
+
+        return jsonify({"message": "Round successfully updated", "data": round}), 200
+    except Exception as e:
+        return jsonify({"error": "Internal Server Error", "message": f"ERROR: {e}"}), 500
+    
+        
